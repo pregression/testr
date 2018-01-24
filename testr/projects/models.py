a73d291ac7a01ab6af4f358dd3e27262c84a51e2
Evan Duncan <evan.e.duncan@gmail.com>
2018-01-23 21:22:54 -0600

    (feat) Add some tests and functionality to the projects model

--- testr/projects/models.py
+++ testr/projects/models.py
@@ -1,5 +1,30 @@
 from django.db import models
+from django.utils.timezone import now
+
+from custom_auth.models import User
 
 
 class Project(models.Model):
     name = models.CharField(max_length=255, db_index=True)
+    users = models.ManyToManyField(User)
+    is_active = models.BooleanField(default=True)
+    deactivated_at = models.DateTimeField(null=True)
+    activated_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    def deactivate(self):
+        self.is_active = False
+        self.deactivated_at = now()
+        self.save()
+        return self
+
+    def activate(self):
+        self.is_active = True
+        self.activated_at = now()
+        self.save()
+        return self
+
+    def is_abandoned(self):
+        return len(self.users.all()) == 0
---
+++ testr/projects/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+
+from projects.models import Project
+
+
+class ProjectModelTests(TestCase):
+    def setUp(self):
+        active_project = Project(name="foo")
+        inactive_project = Project(name="bar", is_active=False)
+        active_project.save()
+        inactive_project.save()
+        self.active_project = active_project
+        self.inactive_project = inactive_project
+
+    def test_is_active_defaults_to_true(self):
+        self.assertTrue(self.active_project.is_active)
+
+    def test_can_be_marked_inactive_when_created(self):
+        self.assertFalse(self.inactive_project.is_active)
+
+    def test_project_can_be_deactivated(self):
+        self.active_project.deactivate()
+        self.assertFalse(self.active_project.is_active)
+
+    def test_can_see_when_project_was_deactivated(self):
+        self.active_project.deactivate()
+        self.assertIsNotNone(self.active_project.deactivated_at)
+
+    def test_project_can_be_activated(self):
+        self.inactive_project.activate()
+        self.assertTrue(self.inactive_project.is_active)
+
+    def test_can_see_when_project_was_activated(self):
+        self.inactive_project.activate()
+        self.assertIsNotNone(self.inactive_project.activated_at)
+
+    def test_can_tell_if_project_has_been_abandoned(self):
+        self.assertTrue(self.active_project.is_abandoned())
+
+    def test_string_representation_includes_name(self):
+        self.assertEqual(self.active_project.name, str(self.active_project))
