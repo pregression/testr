1286a8de6b10c74814a68441739773cbbdcd1c86
Evan Duncan <evan.e.duncan@gmail.com>
2018-02-11 19:19:28 -0600

    Add gzip to statifiles pipeline

--- testr/settings.py
+++ testr/settings.py
@@ -196,6 +196,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
