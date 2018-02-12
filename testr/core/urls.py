fa35bba59243427ef132d66a2bd23df38173de4d
Evan Duncan <evan.e.duncan@gmail.com>
2018-02-11 21:21:27 -0600

    Add terms and privacy pages

--- testr/core/urls.py
+++ testr/core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', include('testr.marketing.urls')),
-    path('license/', views.gpl_v3, name='license')
+    path('license/', views.gpl_v3, name='license'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
 ]
--- testr/core/views.py
+++ testr/core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def gpl_v3(request):
     return render(request, 'core/license.html', {})
+
+
+def terms(request):
+    return render(request, 'core/terms.html', {})
+
+
+def privacy(request):
+    return render(request, 'core/privacy.html', {})
