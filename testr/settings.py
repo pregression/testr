44bacbedbaffb231920ad197273c27c119ed681c
Evan Duncan <evan.e.duncan@gmail.com>
2018-02-11 20:33:37 -0600

    Debugging some more

--- testr/settings.py
+++ testr/settings.py
@@ -196,7 +196,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
