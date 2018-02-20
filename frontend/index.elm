23f23aa524f80fd08e95cd3069bd4aec5bf4a3a3
Evan Duncan <evan.e.duncan@gmail.com>
2018-02-20 00:18:18 -0600

    Initial include elm-lang

---
+++ frontend/index.elm
@@ -0,0 +1,17 @@
+import Html exposing (..)
+
+-- MODEL
+type alias Model = { }
+
+-- UPDATE
+type Msg = Reset 
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Reset -> model
+
+-- VIEW
+
+-- view : Model -> Html Msg
+-- view model = 
